67b6ee0837f1d4394496214b1efd993b0f39addd
Chat Book <chatbookai@gmail.com>
2024-10-09 17:56:50 -0700

    PPTX

--- htdocs/api/ai/pptx/pptx.php
+++ htdocs/api/ai/pptx/pptx.php
@@ -1,4 +1,6 @@
 <?php
+header("Content-Type: application/json");
+
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 require_once('../../config.inc.php');
@@ -6,48 +8,76 @@ require_once('../../adodb5/adodb.inc.php');
 require_once("../../vendor/autoload.php");
 
 
-// with your own install
-\PhpOffice\PhpPresentation\Autoloader::register();
-\PhpOffice\Common\Autoloader::register();
+$JsonContent      = file_get_contents("./json/0001.json");
+$Array            = json_decode($JsonContent, true);
+
+$首页             = $Array['pages'][0];
+$首页['children'][0]['children'][0]['children'];
+$首页['children'][0]['children'][0]['children'][0]['text'] = "PPT标题";
+$首页['children'][1]['children'][0]['children'][0]['text'] = "汇报人";
+//print_R($首页['children'][1]['children'][0]['children'][0]['text']);
+
+$目录			= $Array['pages'][1];
+$目录Children 	= (array)$目录['children'];
+//$目录['children'][0]['children'][0]['children'];
+//$目录['children'][0]['children'][0]['children'][0]['text'] = "PPT标题";
+//$目录['children'][1]['children'][0]['children'][0]['text'] = "汇报人";
+foreach($目录Children as $目录ChildrenItem)  {
+	if($目录ChildrenItem['children'][0]['children'][0]['text']!="")  {
+		//print_R($目录ChildrenItem['children'][0]['children'][0]['text']);
+		//print_R($目录ChildrenItem['point']);
+	}
+	//print $目录ChildrenItem['children'][0]['children'][0]['text']."\n";
+}
+//print_R(json_encode($目录Children));
+
+$章节封面	= $Array['pages'][2];
+//print_R($章节封面['title']);
+foreach($章节封面['children'] as $章节封面ChildrenItem)  {
+	//print $章节封面ChildrenItem['children'][0]['children'][0]['text']."\n";
+}
+//print_R(json_encode($章节封面));
+
+$内容页	= $Array['pages'][3];
+//print_R($内容页['title']);
+foreach($内容页['children'] as $内容页ChildrenItem)  {
+	//print $内容页ChildrenItem['children'][0]['children'][0]['text']."\n";
+	//print $内容页ChildrenItem['children'][1]['children'][0]['text']."\n";
+}
+//print_R(json_encode($内容页));
 
 
-use PhpOffice\PhpPresentation\PhpPresentation;
-use PhpOffice\PhpPresentation\IOFactory;
-use PhpOffice\PhpPresentation\Style\Color;
-use PhpOffice\PhpPresentation\Style\Alignment;
+$内容页	= $Array['pages'][4];
+//print_R($内容页['title']);
+//print "元素数量：".sizeof($内容页['children'])."\n";
+foreach($内容页['children'] as $内容页ChildrenItem)  {
+	//print $内容页ChildrenItem['children'][0]['children'][0]['text']."\n";
+	//print $内容页ChildrenItem['children'][1]['children'][0]['text']."\n";
+}
+//print_R(json_encode($内容页));
 
-$objPHPPowerPoint = new PhpPresentation();
+$章节封面	= $Array['pages'][5];
+//print_R($章节封面['title']);
+//print "元素数量：".sizeof($章节封面['children'])."\n";
+foreach($章节封面['children'] as $章节封面ChildrenItem)  {
+	//print $章节封面ChildrenItem['children'][0]['children'][0]['text']."\n";
+}
 
-// Create slide
-$currentSlide = $objPHPPowerPoint->getActiveSlide();
 
-// Create a shape (drawing)
-$shape = $currentSlide->createDrawingShape();
-$shape->setName('PHPPresentation logo')
-      ->setDescription('PHPPresentation logo')
-      ->setPath('./images/logo.jpg')
-      ->setHeight(36)
-      ->setOffsetX(10)
-      ->setOffsetY(10);
-$shape->getShadow()->setVisible(true)
-                   ->setDirection(45)
-                   ->setDistance(10);
-
-// Create a shape (text)
-$shape = $currentSlide->createRichTextShape()
-      ->setHeight(300)
-      ->setWidth(600)
-      ->setOffsetX(170)
-      ->setOffsetY(180);
-$shape->getActiveParagraph()->getAlignment()->setHorizontal( Alignment::HORIZONTAL_CENTER );
-$textRun = $shape->createTextRun('Thank you for using PHPPresentation!');
-$textRun->getFont()->setBold(true)
-                   ->setSize(60)
-                   ->setColor( new Color( 'FFE06B20' ) );
-
-$oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
-$oWriterPPTX->save(__DIR__ . "/output/sample.pptx");
-$oWriterODP = IOFactory::createWriter($objPHPPowerPoint, 'ODPresentation');
-$oWriterODP->save(__DIR__ . "/output/sample.odp");
+$所有页面 = (array)$Array['pages'];
+$PageId = 1;
+foreach($所有页面 as $单个页面) 	{
+	$有效元素数量 = 0;
+	print "\n\n----------------------------------------------------------\n";
+	print "PageId: $PageId 元素数量：".sizeof($单个页面['children'])."\n";
+	foreach($单个页面['children'] as $单个页面ChildrenItem)  {
+		if($单个页面ChildrenItem['children'][0]['children'][0]['text']!="")  {
+			print $单个页面ChildrenItem['children'][0]['children'][0]['text']."\n";
+			$有效元素数量 ++;
+		}
+	}
+	print "有效元素数量: $有效元素数量\n";
+	$PageId ++ ;
+}
 
 ?>
