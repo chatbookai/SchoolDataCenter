7a5f28c4becec7d4bea8c1c08225f1fd25de9fea
Chat Book <chatbookai@gmail.com>
2024-10-17 18:02:22 -0700

    htdocs/src/configs/auth.ts

---
+++ htdocs/src/configs/auth.ts
@@ -0,0 +1,47 @@
+import os from 'os'
+
+const hostname = os.hostname()
+
+let APP_URL = '/api/'
+let themeNameTemp = "数据中心"
+let indexDashboardPath = "/dashboards/analytics"
+let indexMenuspath = "auth/menus.php"
+let indexImageUrl = '/images/pages/auth-v2-login-illustration-light.png'
+let logoUrl = '/images/pages/auth-v2-login-illustration-light.png'
+let apiEncryptKey = 'fbae1da1c3f10b1ce0c75c8f5d3319d0'
+
+if(hostname == 'localhost' || hostname == '127.0.0.1')   {
+  APP_URL = "http://localhost:80/api/"
+  themeNameTemp = "单点职校数据中心"
+  indexDashboardPath = "/dashboards/analytics"
+  indexMenuspath = "auth/menus.php"
+  indexImageUrl = '/images/pages/auth-v2-login-illustration-light.png'
+  logoUrl = '/images/pages/auth-v2-login-illustration-light.png'
+  apiEncryptKey = 'fbae1da1c3f10b1ce0c75c8f5d3319d0'
+}
+
+if(hostname == '110.90.174.66' || 1)   {
+  themeNameTemp = "福鼎职校"
+  indexDashboardPath = "/dashboards/analytics"
+  indexMenuspath = "auth/menus.php"
+  indexImageUrl = '/images/school/fdzz/index.jpg'
+  logoUrl = '/images/school/fdzz/logo.png'
+  apiEncryptKey = 'fbae1da1c3f10b1ce0c75c8f5d3319d0'
+}
+
+export default {
+  meEndpoint: APP_URL+'jwt.php?action=refresh',
+  loginEndpoint: APP_URL+'jwt.php?action=login',
+  logoutEndpoint: APP_URL+'jwt.php?action=logout',
+  refreshEndpoint: APP_URL+'jwt.php?action=refresh',
+  registerEndpoint: APP_URL+'jwt/register',
+  storageTokenKeyName: 'accessToken',
+  onTokenExpiration: 'refreshToken', // logout | refreshToken
+  backEndApiHost: APP_URL,
+  themeName: themeNameTemp,
+  indexDashboardPath: indexDashboardPath,
+  indexMenuspath: indexMenuspath,
+  k: apiEncryptKey,
+  indexImageUrl: indexImageUrl,
+  logoUrl: logoUrl
+}
