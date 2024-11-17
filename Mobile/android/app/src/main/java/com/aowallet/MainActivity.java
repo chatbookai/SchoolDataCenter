3abd4b0c86d7eefa60c60411f2c8a48c69e8ecd7
Chat Book <chatbookai@gmail.com>
2024-11-17 14:25:08 -0800

    Delete Mobile

--- Mobile/android/app/src/main/java/com/aowallet/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package net.dandian.dc;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- Mobile/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    reactStrictMode: false
-};
-
-export default nextConfig;
-
